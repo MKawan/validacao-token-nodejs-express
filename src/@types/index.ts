748ed450ed062762aab01adbd67196e159ca31ba
Mateus Kawan <mateus77kawan@gmail.com>
2024-01-11 21:40:42 -0300

    atualizando

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,13 +1,24 @@
 "use strict"
 
 import  express, {Request, Response, NextFunction}  from "express";
+import usersRoute from "../routes/users.route";
+import statusRoute from "../routes/status.route";
 
 const app = express();
-app.get('/status', (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({
-        foo: 'bar'
-    })
-});
+
+//configuração da aplicação
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// configuração de Rotas
+app.use(usersRoute);
+app.use(statusRoute);
+
+// app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+//     res.status(200).send({
+//         foo: 'bar'
+//     })
+// });
 app.listen(3000, () =>{
     console.log('Aplicação executando na porta: 3000!');
 })
